1c01249e85cfb95ac76ecfe9b12fb3669510e236
RayL <ray.li.cmd@gmail.com>
2019-09-15 00:17:40 +0800

    add bytesToTrits util

--- src/util/byte.ts
+++ src/util/byte.ts
@@ -86,10 +86,37 @@ export namespace Bytes {
   };
 
   /**
+   * Transfer Each Octet into 5-Trinary
+   * @param bytes The Byte Array Input
+  export function bytesToTrits(bytes: Uint8Array) {
+    let output = new Array<number>();
+    for (const byte of bytes) {
+      if (byte >= 243 || byte < 0) throw 'Invalid Byte Number (0 <= x < 243)';
+      const trits = byte.toString(3);
+      for (let trit of trits) {
+        output.push(parseInt(trit));
+      }
+    }
+    return output;
+  };
+
+  export function tritsToBytes(trits: Array<number>) {
+
+  };
+  */
+
+  /**
    * Transfer Octets Array into Trinary Array
    * @param bytes The Byte Array Input
    */
-  export function bytesToTrits(bytes: Uint8Array) {
+  export function bytesToTrits(bytes: Uint8Array, correction?: Array<number>) {
+    const table: { [key: string]: Array<number> } = {
+      '000': [0, 0], '001': [0, 1],
+      '010': [0, -1], '011': [1, 0],
+      '100': [1, 1], '101': [1, -1],
+      '110': [-1, 0], '111': [-1, 1],
+    };
+
     let trits = new Array<number>();
 
     // Pad Zero(s) if bytes not divided by 3
@@ -101,6 +128,7 @@ export namespace Bytes {
     let length = Math.floor(bytes.length * 8 / 3) * 2;
 
     let counter = 0;
+    let pointer = 0;
 
     while (length >= 16) {
       // get next three octets to bits
@@ -115,30 +143,19 @@ export namespace Bytes {
       // Transform each 3-bits into 2 trits
       while (bits24.length > 0) {
         let bits3 = bits24.splice(0, 3);
-        // {0, 0, 0} -> {0, 0}
-        if (bits3[0] === false && bits3[1] === false && bits3[2] === false)
-          trits.push(0, 0);
-        // {0, 0, 1} -> {0, 1}
-        else if (bits3[0] === false && bits3[1] === false && bits3[2] === true)
-          trits.push(0, 1);
-        // {0, 1, 0} -> {0, -1}
-        else if (bits3[0] === false && bits3[1] === true && bits3[2] === false)
-          trits.push(0, -1);
-        // {0, 1, 1} -> {1, 0}
-        else if (bits3[0] === false && bits3[1] === true && bits3[2] === true)
-          trits.push(1, 0);
-        // {1, 0, 0} -> {1, 1}
-        else if (bits3[0] === true && bits3[1] === false && bits3[2] === false)
-          trits.push(1, 1);
-        // {1, 0, 1} -> {1, -1}
-        else if (bits3[0] === true && bits3[1] === false && bits3[2] === true)
-          trits.push(1, -1);
-        // {1, 1, 0} -> {-1, 0}
-        else if (bits3[0] === true && bits3[1] === true && bits3[2] === false)
-          trits.push(-1, 0);
-        // {1, 1, 1} -> {-1, 1}
-        else if (bits3[0] === true && bits3[1] === true && bits3[2] === true)
-          trits.push(-1, 1);
+        const k0 = bits3[0] === true ? '1' : '0';
+        const k1 = bits3[1] === true ? '1' : '0';
+        const k2 = bits3[2] === true ? '1' : '0';
+        const key = k0 + k1 + k2;
+
+        // Correction for Failed trits
+        if (correction) {
+          if (correction.indexOf(pointer) > -1 && key === '111') {
+            trits.push(...[-1, -1]);
+          } else trits.push(...table[key]);
+        } else trits.push(...table[key]);
+        
+        pointer += 2;
       }
 
       length -= 16;
@@ -147,6 +164,51 @@ export namespace Bytes {
     
     return trits;
   };
+
+  /*
+  export function tritsToBytes(trits: Array<number>) {
+
+    const table: { [key: string]: Array<number> } = {
+      // Original Ternary Transformation
+      '00': [0,0,0], '01': [0,0,1], '0-1': [0,1,0],
+      '10': [0,1,1], '11': [1,0,0], '1-1': [1,0,1],
+      '-10': [1,1,0], '-11': [1,1,1],
+      // Modular Ternary Transformation
+      '02': [0,1,0], '12': [1,0,1], '20': [1,1,0],
+      '21': [1,1,1], '22': [1,1,1], '-1-1': [1,1,1],
+    };
+
+    // 5-trits per byte
+
+    const padding = bits % 8 > 4 ? bits % 8 : (8 - bits % 8);
+    if (Math.floor((trits.length / 2) * 3) % 8 !== 0) throw 'trits cannot be divided as bytes';
+    // Check is trits first
+    for (let i = 0 ; i < trits.length ; i++) {
+      if (trits[i] < -1 || trits[i] > 2) throw `Trit[${i}] = ${trits[i]} is invalid`;
+    }
+
+    let output = Array<number>();
+    let correction = Array<number>();
+
+    let pointer = 0;
+    while (trits.length > 0) {
+      let bits24 = Array<number>();
+      // Get 24-bits
+      for (let i = 0 ; i < 3 ; i++) {
+        let t = trits.splice(0, 2);
+        let key = t[0].toString() + t[1].toString();
+        if (key === '22' || key === '-1-1') correction.push(pointer);
+        bits24.push(...table[key]);
+        pointer += 2;
+      }
+
+      for (let i = 0 ; i < 3 ; i++)
+        output.push(parseInt(bits24.splice(0, 8).join(''), 2));
+    }
+
+    return { data: Buffer.from(new Uint8Array(output)), correction };
+  }
+  */
 
   export function concatBytes(a: Uint8Array, b: Uint8Array) { // a, b TypedArray of same type
     var c = new Uint8Array(a.length + b.length);
